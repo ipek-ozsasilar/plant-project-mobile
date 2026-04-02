0a03ef9f012c15f652ddf0c9fb46f277100c1796
ipek-ozsasilar <ipek.ozsslr@gmail.com>
2026-04-02 23:51:18 +0300

    Add date formatting initialization for Turkish locale

--- lib/core/initializers/app_initializers.dart
+++ lib/core/initializers/app_initializers.dart
@@ -1,5 +1,6 @@
 import 'package:bitirme_mobile/service_locator/service_locator.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 /// Uygulama başlamadan önce çalıştırılması gereken kurulumlar.
 final class AppInitializers {
@@ -7,6 +8,8 @@ final class AppInitializers {
 
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    // DateFormat(..., 'tr') için yerel tarih/sayı biçim verisi
+    await initializeDateFormatting('tr', null);
     await setupServiceLocator();
   }
 }
